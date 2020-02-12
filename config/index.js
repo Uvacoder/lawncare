1c1fa8051a1d789efbef986b51fcdb5eda83bcf2
Adam Gapper <agapper@gappsapps.co.uk>
2020-02-12 23:37:27 +0000

    updated hellodave.club to nearandnow.services

--- config/index.js
+++ config/index.js
@@ -1,14 +1,14 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. "
 
-  siteTitle: 'hellodave.club', // Navigation and Site Title
-  siteTitleAlt: 'hellodave.club - services', // Alternative Site title for SEO
-  siteTitleShort: 'hellodave.club', // short_name for manifest
+  siteTitle: 'nearandnow.services', // Navigation and Site Title
+  siteTitleAlt: 'nearandnow.services - services', // Alternative Site title for SEO
+  siteTitleShort: 'nearandnow.services', // short_name for manifest
   siteHeadline: 'Take a look at our services and see our recent projects.', // Headline for schema.org JSONLD
-  siteUrl: 'https://hellodave.club', // Domain of your site. No trailing slash!
+  siteUrl: 'https://nearandnow.services', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo.png', // Used for SEO and manifest
-  siteDescription: 'hellodave.club',
+  siteDescription: 'nearandnow.services',
   author: 'gappsapps', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
--- src/cms/cms.tsx
+++ src/cms/cms.tsx
@@ -5,6 +5,7 @@ import cloudinary from 'netlify-cms-media-library-cloudinary'
 import ContactusPreview from './preview-templates/ContactusPreview'
 import OurservicePreview from './preview-templates/OurservicePreview'
 import WebpagePreview from './preview-templates/WebpagePreview'
+import ProjectPreview from './preview-templates/ProjectPreview'
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
@@ -12,3 +13,4 @@ CMS.registerMediaLibrary(cloudinary)
 CMS.registerPreviewTemplate('contactus', ContactusPreview)
 CMS.registerPreviewTemplate('service', OurservicePreview)
 CMS.registerPreviewTemplate('webpage', WebpagePreview)
+CMS.registerPreviewTemplate('webpage', ProjectPreview)
