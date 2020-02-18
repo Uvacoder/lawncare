b07c0e67dd1b5f7920cb7b969e0acb4e019a8892
Adam Gapper <agapper@gappsapps.co.uk>
2020-02-18 15:52:48 +0000

    Update ServicePreview.tsx

--- src/cms/preview-templates/ServicePreview.tsx
+++ src/cms/preview-templates/ServicePreview.tsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { ServicePageTemplate } from '../../templates/service'
+import { ServicePage } from '../../templates/service'
 
 
 
 const ServicePreview = ({ entry, getAsset }) => {
-  const entryBlurbs = entry.getIn(['data', 'intro', 'blurbs'])
-  const blurbs = entryBlurbs ? entryBlurbs.toJS() : []
-
-  const entryTestimonials = entry.getIn(['data', 'testimonials'])
-  const testimonials = entryTestimonials ? entryTestimonials.toJS() : []
-
-  const entryPricingPlans = entry.getIn(['data', 'pricing', 'plans'])
-  const pricingPlans = entryPricingPlans ? entryPricingPlans.toJS() : []
 
   return (
          <div>
