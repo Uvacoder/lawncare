1fff41f52bbcaae5667518064068496efaf72e2b
Adam Gapper <agapper@gappsapps.co.uk>
2020-03-14 17:44:59 +0000

    moved recommendation pics

--- src/cms/cms.tsx
+++ src/cms/cms.tsx
@@ -4,9 +4,11 @@ import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import ContactusPreview from './preview-templates/ContactusPreview'
 import PagePreview from './preview-templates/PagePreview'
+import BlogPreview from './preview-templates/BlogPreview'
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('contactus', ContactusPreview)
 CMS.registerPreviewTemplate('page', PagePreview)
+CMS.registerPreviewTemplate('post', BlogPreview)
---
+++ src/cms/preview-templates/BlogPreview.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import BlogPage from '../../templates/post'
+
+
+
+const BlogPreview = ({ entry, getAsset }) => {
+
+  return (
+         <div>
+    <BlogPage
+      slug={entry.getIn(['data', 'string'])}
+      title={entry.getIn(['data', 'string'])}
+      featuredimage={entry.getIn(['data', 'image'])}
+      featuredimage_alt={entry.getIn(['data', 'string'])}
+      body={entry.getIn(['data', 'markdown'])}
+      tags={entry.getIn(['data', 'list'])}
+    /></div>
+  )
+}
+
+BlogPreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  getAsset: PropTypes.func,
+}
+
+export default BlogPreview
